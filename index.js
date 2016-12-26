a25b862cd6db627572e564f1091188475be16a5a
rajashekar <rajashekarkhallel@gmail.com>
2016-12-26 18:43:26 +0530

    initial update

---
+++ index.js
@@ -0,0 +1,46 @@
+/*var apiai = require('apiai');
+ 
+var app = apiai("68d79c9d5c5a42e1a4395b72ed58f6ac");
+ 
+var request = app.textRequest('how much did john scored', {
+    sessionId: '123456'
+});
+ 
+request.on('response', function(response) {
+    console.log(response);
+});
+ 
+request.on('error', function(error) {
+    console.log(error);
+});
+ 
+request.end();*/
+
+var express = require('express');
+var app = express();
+
+//heroku
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+});
+
+var server = app.listen(app.get('port'), function () {
+
+   //var host = server.address().address
+  // var port = server.address().port
+	console.log('Node app is running on port', app.get('port'));
+  // console.log("Example app listening at http://%s:%s", host, port)
+});
+
+
+/*app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});*/
